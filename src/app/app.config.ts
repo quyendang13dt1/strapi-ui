296d169b4e8aa546d0730a6c00bcbe978b6f90ad
Quyen Dang <qdang@atomiton.com>
2025-02-18 11:08:09 +0700

    update build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withInMemoryScrolling} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http'; 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient()]
+  providers: [
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(
+          routes,
+          withInMemoryScrolling({
+            scrollPositionRestoration: 'top'
+          })
+      ),
+      provideAnimations(),
+      provideHttpClient()
+    ]
 };
 
-export const URL_API = 'http://localhost:1337/api/';
-export const URL_IMAGE = 'http://localhost:1337';
+export const URL_API = 'https://strapi-project-32d4.onrender.com/api/';
+export const URL_IMAGE = 'https://strapi-project-32d4.onrender.com';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,11 @@
 import { Routes } from '@angular/router';
-import { ArticleDetailComponent } from './article-detail/article-detail.component';
-import { ArticleListComponent } from './article-list/article-list.component';
 import { KnowledgeHubComponent } from './knowledge-hub/knowledge-hub.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'knowledge-hub',
-    pathMatch: 'full',
-  },
-  {
-    path: 'knowledge-hub',
-    component: KnowledgeHubComponent,
-  },
-  {
-    path: 'list',
-    component: ArticleListComponent,
-  },
-  {
-    path: 'detail/:id',
-    component: ArticleDetailComponent,
-  },
-  // {
-  //     path: '**',
-  //     redirectTo: '',
-  //     pathMatch: 'full',
-  // },
+  { path: '', redirectTo: 'knowledge-hub', pathMatch: 'full' },
+  { path: 'knowledge-hub', component: KnowledgeHubComponent },
+  { path: 'list', component: ArticleListComponent },
+  { path: 'detail/:id', component: ArticleDetailComponent },
 ];
