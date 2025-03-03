573c9eb71ec83b59d2288328c38830fefcaefce5
Quyen Dang <qdang@atomiton.com>
2025-03-03 12:20:06 +0700

    fix detail article

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -31,54 +31,49 @@ export class ArticleDetailComponent {
     this.id = this.route.snapshot.paramMap.get('id')!;
     this.isLoading = true;
     combineLatest([
-      this._ArticleService
-      .getArticleDetail(
+      this._ArticleService.getArticleDetail(
         this.id,
         this._ActivatedRoute.snapshot.queryParamMap.get('status')
       ),
-      this._ArticleService
-      .getArticleDetailRelation(
+      this._ArticleService.getArticleDetailRelation(
         this.id,
         this._ActivatedRoute.snapshot.queryParamMap.get('status')
-      )
-    ])
-      .subscribe(([rs1, rs2]: any) => {
-        this.detail = rs1.data;
-        this.detailRelation = rs2?.data;
-        const splitParts = this.detail?.Content?.split(
-          /<pre><code class="language-plaintext">|<\/code><\/pre>/
-        );
+      ),
+    ]).subscribe(([rs1, rs2]: any) => {
+      this.detail = rs1.data;
+      this.detailRelation = rs2?.data;
+      const splitParts = this.detail?.Content?.split(
+        /<pre><code class="language-plaintext">|<\/code><\/pre>/
+      );
 
-        // Thay thế &lt; → < và &gt; → >
-        this.innerHTMLData = splitParts
-          ?.map((part: any) => {
-            return {
-              type: this.getType(part),
-              src: part.match(/src="([^"]+)"/)?.[1],
-              style: part.match(/style="([^"]+)"/)?.[1],
-              data: part
-                .replace(
-                  /<oembed url="https:\/\/www\.youtube\.com\/watch\?v=([^"]+)"><\/oembed>/g,
-                  `<iframe width="560" height="315" 
-                    src="https://www.youtube.com/embed/qLs_Ybqq324" 
-                    frameborder="0" allowfullscreen></iframe>`
-                )
-                .replace('www.youtube.com/watch', 'www.youtube.com/embed'),
-            };
-          })
-          .filter((x: any) => x?.data);
+      // Thay thế &lt; → < và &gt; → >
+      this.innerHTMLData = splitParts
+        ?.map((part: any) => {
+          return {
+            type: this.getType(part),
+            src: part.match(/src="([^"]+)"/)?.[1],
+            style: part.match(/style="([^"]+)"/)?.[1],
+            data: part
+              .replace(
+                /<oembed url="(.*?)"><\/oembed>/g,
+                `<iframe style="  width: 100%; max-width: 800px;height: 450px; border: none;" src="$1" frameborder="0" allowfullscreen></iframe>`
+              )
+              .replace('www.youtube.com/watch', 'www.youtube.com/embed'),
+          };
+        })
+        .filter((x: any) => x?.data);
 
-        // this.innerHTMLData = innerHTMLData?.concat([
-        //   {
-        //     type: 'iframe',
-        //     src: 'https://www.youtube.com/watch?v=flmiFlJVdmA',
-        //     style: '',
-        //     data: `<iframe src=\"https://www.youtube.com/watch?v=flmiFlJVdmA\"></iframe>`,
-        //   },
-        // ]);
-        console.log('this.innerHTMLData', this.detail, rs2);
-        this.isLoading = false;
-      });
+      // this.innerHTMLData = innerHTMLData?.concat([
+      //   {
+      //     type: 'iframe',
+      //     src: 'https://www.youtube.com/watch?v=flmiFlJVdmA',
+      //     style: '',
+      //     data: `<iframe src=\"https://www.youtube.com/watch?v=flmiFlJVdmA\"></iframe>`,
+      //   },
+      // ]);
+      console.log('this.innerHTMLData', this.detail, rs2);
+      this.isLoading = false;
+    });
   }
 
   getType(part: any) {
